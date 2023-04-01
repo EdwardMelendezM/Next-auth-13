ee60ea57a7c92d29aa33b7d9fc8745f598b610dd
EdwardMelendezM <106676335+EdwardMelendezM@users.noreply.github.com>
2023-04-01 10:43:10 -0500

    adding meta datos for blog page

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,11 +1,11 @@
 import Logo from './Logo'
 export default function Header () {
   return (
-    <nav className='border-b-4 border-gray-100'>
+    <header className='border-b-4 border-gray-100'>
       <div className='container mx-auto px-4 sm:px-6 py-4'>
         {/* Aqui estaara el logo */}
         <Logo />
       </div>
-    </nav>
+    </header>
   )
 };
